2f722739a09ef2c5c95d22e0b351c689e8b42001
HaniffHazmi <haniffhazmi1@gmail.com>
2025-07-04 01:08:00 +0800

    Navbar halfway done

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggle_menu() {
+  const menu = document.querySelector(".menu__links");
+  const icon = document.querySelector(".hamburger__icon");
+
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
